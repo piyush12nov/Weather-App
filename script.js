6f290cb48ed90ab4db6b9099e14b1ec901c6d9b0
Piyush Singh <108541125+piyush12nov@users.noreply.github.com>
2023-07-29 04:09:15 +0530

    script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ async function checkWeather(city) {
 	if (response.status == 404) {
 		document.querySelector('.error').style.display == 'block';
 		document.querySelector('.weather').style.display == 'none';
-	} 
-    else {
+	}
+	else {
 		var data = await response.json();
 
 		document.querySelector('.city').innerHTML = data.name;
